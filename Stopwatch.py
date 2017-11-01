0f8de11e93af7c9b4c7a69b2bd14878a0a46862d
mleousi <mleousi24@gmail.com>
2017-11-01 03:10:12 +0200

    Update and rename user39_p6lcklqfjz_2_STOPWATCH.py to Stopwatch.py

--- Stopwatch.py
+++ Stopwatch.py
@@ -2,7 +2,7 @@
 
 import simplegui
 
-# define global variables
+#global variables
 k = 0
 count = 0
 success = 0
@@ -10,8 +10,8 @@ rate = str(success) + '/' + str(count)
 runs = False
 
 
-# define helper function format that converts time
-# in tenths of seconds into formatted string A:BC.D
+#helper function format that converts time
+#in tenths of seconds into formatted string A:BC.D
 def format(t):
     msec = t % 10
     sec1 = (t / 10)% 10
@@ -19,7 +19,7 @@ def format(t):
     sec2 = (t / 100)% 6
     return str(minutes) + ':' + str(sec2) + str(sec1) + '.' + str(msec)
     
-# define event handlers for buttons; "Start", "Stop", "Reset"
+#event handlers for buttons; "Start", "Stop", "Reset"
 def start():
     global runs
     
@@ -53,32 +53,29 @@ def reset():
     
     
 
-# define event handler for timer with 0.1 sec interval
+#event handler for timer with 0.1 sec interval
 def time_handler():
     global k
     k = k + 1
     
 
-# define draw handler
+#draw handler
 def draw_handler(canvas):    
     canvas.draw_text(format(k), [90, 150], 40, 'White')
     canvas.draw_text(rate, [240 ,35], 30, 'Green')
 
 
     
-# create frame
+#create frame
 frame = simplegui.create_frame("Stopwatch", 300, 300)
 frame.add_button("Start", start, 100)
 frame.add_button("Stop", stop, 100)
 frame.add_button("Reset", reset, 100)
 
-# register event handlers
-
+#register event handlers
 frame.set_draw_handler(draw_handler)
 timer = simplegui.create_timer(100, time_handler)
 
 
-# start frame
+#start frame
 frame.start()
-
-# Please remember to review the grading rubric
